91fb7ef2b6a6eb118732b50f6ec223cc09e3fbab
daniil-b2be <dan.developer@born2be.media>
2025-07-22 11:26:19 +0300

    upd

--- src/shared/ui/kit/Text.vue
+++ src/shared/ui/kit/Text.vue
@@ -1,12 +1,16 @@
 <template>
-  <p :class="['font-normal text-[#32302c]', sizeMap[size], weightMap[weight]]">
+  <p :class="['text-[#32302c]', sizeMap[size], weightMap[weight]]">
     <slot />
   </p>
 </template>
 
 <script lang="ts" setup>
 const sizeMap = { xs: 'text-xs', sm: 'text-sm', base: 'text-base' };
-const weightMap = { '400': 'font-normal', '500': 'font-semibold' };
+const weightMap = {
+  '400': 'font-normal',
+  '500': 'font-medium',
+  '600': 'font-semibold',
+};
 
 withDefaults(
   defineProps<{
